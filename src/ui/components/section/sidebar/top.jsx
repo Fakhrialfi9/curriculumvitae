8744e19e2c692f2c4b2cfb8180dcf8a26fb41d09
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2024-08-07 11:04:24 +0700

    Prepare for deployment

--- src/ui/components/section/sidebar/top.jsx
+++ src/ui/components/section/sidebar/top.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-scroll";
 import sidebarStyle from "../../../../assets/style/components/sidebar.module.css";
 import Logo from "../../../../../public/FavIcon.svg";
 
@@ -5,7 +6,9 @@ const Top = () => {
   return (
     <section className={sidebarStyle.top}>
       <figure className={sidebarStyle.logo}>
-        <img src={Logo}></img>
+        <Link to='profile' smooth={true} duration={350} spy={true} exact='true' offset={-100}>
+          <img src={Logo}></img>
+        </Link>
       </figure>
     </section>
   );
